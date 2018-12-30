3302a2d9d1ebfac83a0a2fbf3f5db0807fe191d0
ravi <ravilamkoti@gmail.com>
2018-12-30 10:03:06 +0530

    My first heroku

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express();
+var socket = require('socket.io')
+
+var server = app.listen(4000,function(){
+    console.log("Listening to the port 4000");
+})
+
+app.use(express.static('public'));
+var io = socket(server)
+io.on('connection',function(socket){
+    console.log(socket.id)
+
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data)
+    });
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data)
+    });
+})
+
